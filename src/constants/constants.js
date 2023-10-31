73d03d561f86deb3d5970250d1faad5c6495ed3e
Ahmed Osman <ahmedaosman00@gmail.com>
2023-10-31 13:59:36 -0700

    Changing the name of whats the weather today to RainOrShine

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,15 @@
 export const projects = [
   {
-    title: "What's the weather today",
+    title: "RainOrShine",
     description:
-      "What's the weather today: A simple and responsive web-based weather app built using HTML, CSS, and JavaScript. Leveraging the OpenWeather API, it provides current weather updates with a clean and straightforward interface. Designed as a testament to my skills in front-end development and API integration.",
+      "RainOrShine: A simple and responsive web-based weather app built using HTML, CSS, and JavaScript. Leveraging the OpenWeather API, it provides current weather updates with a clean and straightforward interface. Designed as a testament to my skills in front-end development and API integration.",
     tags: ["HTML", "CSS", "JavaScript", "OpenWeather API"],
     code: "https://github.com/ahmedaosman-hub/WeatherApp",
     source: "https://ahmedaosman-hub.github.io/WeatherApp/",
     id: 0,
+  },
+  {
+    title: "What do i hav",
   },
 ];
 
