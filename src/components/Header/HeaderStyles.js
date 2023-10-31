c99ddea64f230e4393771c563aeeb6a621fa50f6
Ahmed Osman <ahmedaosman00@gmail.com>
2023-10-31 16:09:17 -0700

    Fixing the nav bar to display contrast to the blackish background

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -2,47 +2,33 @@ import { IoIosArrowDropdown } from "react-icons/io";
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: 1fr;
-  grid-column-gap: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 1rem;
   padding-top: 2rem;
+  background-color: white;
 
   @media ${(props) => props.theme.breakpoints.sm} {
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: repeat(2, 60px);
-    grid-column-gap: 0.5rem;
-    grid-row-gap: 0.5rem;
+    flex-direction: column;
+    align-items: stretch;
   }
 `;
 export const Div1 = styled.div`
-  grid-area: 1 / 1 / 2 / 2;
   display: flex;
-  flex-direction: row;
-  align-content: center;
-  @media ${(props) => props.theme.breakpoints.sm} {
-    grid-area: 1 / 1 / 2 / 3;
-  }
+  align-items: center;
 `;
 export const Div2 = styled.div`
-  grid-area: 1 / 2 / 2 / 4;
-  display: flex;
-  justify-content: space-around;
-  @media ${(props) => props.theme.breakpoints.sm} {
-    grid-area: 2 / 2 / 3 / 5;
-  }
-`;
-export const Div3 = styled.div`
-  grid-area: 1 / 5 / 2 / 6;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  @media ${(props) => props.theme.breakpoints.sm} {
-    align-items: center;
-    grid-area: 1 / 4 / 2 / 6;
-  }
+  flex-wrap: wrap;
+`;
+export const Div3 = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
 `;
 
 // Navigation Links
@@ -50,7 +36,7 @@ export const NavLink = styled.a`
   font-size: 2rem;
   line-height: 32px;
   padding: 0.5rem 1rem;
-  color: rgba(255, 255, 255, 0.75);
+  color: black;
   transition: 0.4s ease;
   &:hover {
     color: #fff;
@@ -113,6 +99,7 @@ export const NavProductsIcon = styled(IoIosArrowDropdown)`
 export const SocialIcons = styled.a`
   transition: 0.3s ease;
   color: white;
+  background-color: black;
   border-radius: 50px;
   padding: 8px;
   &:hover {
