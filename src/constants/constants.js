447176e7d825ea28e9b5cfa6d5624619167947df
Ahmed Osman <ahmedaosman00@gmail.com>
2023-10-31 16:16:58 -0700

    Fixing projects code link

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,7 @@ export const projects = [
     description:
       "RainOrShine: A simple and responsive web-based weather app built using HTML, CSS, and JavaScript. Leveraging the OpenWeather API, it provides current weather updates with a clean and straightforward interface. Designed as a testament to my skills in front-end development and API integration.",
     tags: ["HTML", "CSS", "JavaScript", "OpenWeather API"],
-    code: "https://github.com/ahmedaosman-hub/RainOrShine",
+    code: "https://github.com/ahmedaosman-hub/RainOrShine/",
     source: "https://ahmedaosman-hub.github.io/RainOrShine/",
     image: "assets/images/rainorshine.png",
     id: 0,
@@ -14,7 +14,7 @@ export const projects = [
     description:
       "TaskTrail: A responsive task management web app crafted with HTML, CSS, and JavaScript. Features categorized task creation and local data persistence, ensuring tasks remain accessible on revisits. Designed to showcase my proficiency in front-end development and data storage techniques.",
     tags: ["HTML", "CSS", "JavaScript", "Local Storage"],
-    code: "https://github.com/ahmedaosman-hub/TaskTrail",
+    code: "https://github.com/ahmedaosman-hub/TaskTrail/",
     source: "https://ahmedaosman-hub.github.io/TaskTrail/",
     image: "assets/images/tasktrail.png",
     id: 1,
